34d291a0b3aa94894070828875d51d95832b63ad
valentine195 <38669521+valentine195@users.noreply.github.com>
2022-01-04 11:53:43 -0500

    fix: Subheading recursively stringifies properties

--- src/view/ui/Subheading.svelte
+++ src/view/ui/Subheading.svelte
@@ -1,13 +1,36 @@
 <script lang="ts">
     import type { Monster } from "@types";
-    import type {  SubHeadingItem } from "src/data/constants";
+    import type { SubHeadingItem } from "src/data/constants";
+
+    const stringify = (
+        property: Record<string, any> | string | any[] | number | boolean,
+        depth: number = 0
+    ): string => {
+        const ret = [];
+        if (depth == 5) {
+            return "";
+        }
+        if (typeof property == "string") return property;
+        if (typeof property == "number") return `${property}`;
+        if (Array.isArray(property)) {
+            ret.push(
+                `(${property.map((p) => stringify(p, depth++)).join(" ")})`
+            );
+        } else if (typeof property == "object") {
+            for (const value of Object.values(property)) {
+                ret.push(stringify(value, depth++));
+            }
+        }
+        return ret.join(" ");
+    };
 
     export let monster: Monster;
     export let item: SubHeadingItem;
     const subheading: string[] = [];
+
     for (let property of item.properties) {
         if (property in monster) {
-            subheading.push(`${monster[property]}`);
+            subheading.push(`${stringify(monster[property])}`);
         }
     }
 </script>
