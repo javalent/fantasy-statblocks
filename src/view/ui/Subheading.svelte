61cbf0a3d0faeacda187d8e5b8e1f57dc3df2493
valentine195 <38669521+valentine195@users.noreply.github.com>
2022-01-04 14:10:54 -0500

    removed stringify from subheading

--- src/view/ui/Subheading.svelte
+++ src/view/ui/Subheading.svelte
@@ -1,28 +1,7 @@
 <script lang="ts">
     import type { Monster } from "@types";
     import type { SubHeadingItem } from "src/data/constants";
-
-    const stringify = (
-        property: Record<string, any> | string | any[] | number | boolean,
-        depth: number = 0
-    ): string => {
-        const ret = [];
-        if (depth == 5) {
-            return "";
-        }
-        if (typeof property == "string") return property;
-        if (typeof property == "number") return `${property}`;
-        if (Array.isArray(property)) {
-            ret.push(
-                `(${property.map((p) => stringify(p, depth++)).join(" ")})`
-            );
-        } else if (typeof property == "object") {
-            for (const value of Object.values(property)) {
-                ret.push(stringify(value, depth++));
-            }
-        }
-        return ret.join(" ");
-    };
+    import { stringify } from "../utils";
 
     export let monster: Monster;
     export let item: SubHeadingItem;
