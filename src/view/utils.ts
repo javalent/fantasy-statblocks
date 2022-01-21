85002342bd924bff4c8a1c4306c192ad89fead02
valentine195 <38669521+valentine195@users.noreply.github.com>
2022-01-21 10:07:57 -0500

    adds stringifyWithKeys method to fully stringify object

--- src/view/utils.ts
+++ src/view/utils.ts
@@ -18,3 +18,28 @@ export const stringify = (
     }
     return ret.join(" ");
 };
+export const stringifyWithKeys = (
+    property: Record<string, any> | string | any[] | number | boolean,
+    depth: number = 0
+): string => {
+    const ret = [];
+    if (depth == 5) {
+        return "";
+    }
+    if (!property || property == null) return ``;
+    if (typeof property == "string") return property;
+    if (typeof property == "number") return `${property}`;
+    if (Array.isArray(property)) {
+        ret.push(
+            `${property.map((p) => stringifyWithKeys(p, depth++)).join(" ")}`
+        );
+    } else if (typeof property == "object") {
+        for (const [key, value] of Object.entries(property)) {
+            ret.push(
+                stringifyWithKeys(key, depth++),
+                stringifyWithKeys(value, depth++)
+            );
+        }
+    }
+    return ret.join(" ");
+};
