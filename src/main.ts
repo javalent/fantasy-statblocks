fdd8436518dac9ac1c532bf683530ccd5f75d756
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2022-01-21 22:43:45 -0500

    re-import css

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ import type { Monster, StatblockParameters, Trait } from "@types";
 import StatblockSettingTab from "./settings/settings";
 import fastCopy from "fast-copy";
 
-/* import "./main.css"; */
+import "./main.css";
 import { sort } from "fast-sort";
 import type { Plugins } from "../../obsidian-overload";
 import type { HomebrewCreature } from "../../obsidian-initiative-tracker/@types";
