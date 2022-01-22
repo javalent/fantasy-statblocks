13ca6473d1047bd33fe9682f2b38902a3ce29576
Dmitriy Dovgan <d_dovgan@wargaming.net>
2022-01-22 18:42:13 +0200

    Add suport for new 5etools placeholders

--- src/importers/5eToolsImport.ts
+++ src/importers/5eToolsImport.ts
@@ -11,16 +11,23 @@ const abilityMap: { [key: string]: string } = {
 
 function parseString(str: string) {
     return str
-        .replace(/\{\@condition (.+?)}/g, "$1")
-        .replace(/\{\@spell ([\s\S]+?)\}/g, `$1`)
-        .replace(/\{\@recharge (.+?)\}/g, `Recharge $1`)
-        .replace(/\{\@h\}/g, ``)
-        .replace(/\{\@damage (.+?)\}/g, `$1`)
-        .replace(/\{\@atk ms\}/g, `Melee Spell Attack`)
-        .replace(/\{\@atk mw\}/g, `Melee Weapon Attack`)
-        .replace(/\{\@atk rw\}/g, `Ranged Weapon Attack`)
-        .replace(/\{\@hit (\d+?)\}/g, `+$1`)
-        .replace(/\{\@dc (\d+?)\}/g, `$1`);
+        .replace(/{@condition (.+?)}/g, "$1")
+        .replace(/{@item (.+?)}/g, "$1")
+        .replace(/{@spell ([\s\S]+?)}/g, `$1`)
+        .replace(/{@recharge (.+?)}/g, `(Recharge $1-6)`)
+        .replace(/{@recharge}/g, `(Recharge 6)`)
+        .replace(/{@h}/g, ``)
+        .replace(/{@damage (.+?)}/g, `$1`)
+        .replace(/{@atk ms}/g, `Melee Spell Attack`)
+        .replace(/{@atk rs}/g, `Ranged Spell Attack`)
+        .replace(/{@atk mw}/g, `Melee Weapon Attack`)
+        .replace(/{@atk rw}/g, `Ranged Weapon Attack`)
+        .replace(/{@atk mw,rw}/g, `Melee / Ranged Weapon Attack`)
+        .replace(/{@creature (.+?)}/g, `$1`)
+        .replace(/{@skill (.+?)}/g, `$1`)
+        .replace(/{@dice (.+?)}/g, `$1`)
+        .replace(/{@hit (\d+?)}/g, `+$1`)
+        .replace(/{@dc (\d+?)}/g, `$1`);
 }
 
 export async function build5eMonsterFromFile(file: File): Promise<Monster[]> {
