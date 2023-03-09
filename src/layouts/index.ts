b7981bcd3b7139a8217fefff3b55c9ad86205452
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-03-09 15:17:56 -0500

    feat!: includes PF2e as basic layout

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,11 +1,14 @@
-import { Layout5e } from "./basic5e";
-import { LayoutFateCore } from "./fateCore";
+import { Layout5e } from "./basic 5e/basic5e";
+import { LayoutFateCore } from "./fate core/fateCore";
+import { LayoutPF2e } from "./pathfinder 2e/pf2e";
 
-export * from "./basic5e";
-export * from "./fateCore";
+export * from "./basic 5e/basic5e";
+export * from "./fate core/fateCore";
+export * from "./pathfinder 2e/pf2e";
 
 export const DefaultLayouts = [
     Layout5e,
     LayoutFateCore,
+    LayoutPF2e
     /** Additional Default Layouts should be added here. */
 ];
