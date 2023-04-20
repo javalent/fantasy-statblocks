8ba0773fdfb662f9c392c74a44b94a9f58d7aeca
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-04-20 08:15:59 -0400

    fix: fix trait overriding

--- src/util/util.ts
+++ src/util/util.ts
@@ -155,7 +155,7 @@ export function transformTraits(
             ("name" in trait || "desc" in trait)
         ) {
             monsterTraits = monsterTraits.filter(
-                (t) => t.name != trait.name && t.desc != trait.desc
+                (t) => t.name != trait.name || t.desc != trait.desc
             );
             monsterTraits.push({
                 ...trait,
