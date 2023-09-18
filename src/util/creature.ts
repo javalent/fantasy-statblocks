45760aa966af6ad4061c45ff27e34d79d65660fe
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-09-18 09:26:12 -0400

    feat: Enables searching by field in creature pane using `@<field> <search>`

--- src/util/creature.ts
+++ src/util/creature.ts
@@ -1,10 +1,12 @@
 import type StatBlockPlugin from "src/main";
 import type { Monster } from "../../index";
 import { SuggestionModal } from "./generic_suggester";
-import type { FuzzyMatch } from "obsidian";
+import type { FuzzyMatch, SearchResult } from "obsidian";
+import { stringify } from "./util";
 
 export class MonsterSuggestionModal extends SuggestionModal<Monster> {
     creature: Partial<Monster>;
+    field: keyof Monster = "name";
     constructor(public plugin: StatBlockPlugin, inputEl: HTMLInputElement) {
         super(plugin.app, inputEl, [...plugin.getBestiaryCreatures()]);
         /* this.onInputChanged(); */
@@ -13,7 +15,15 @@ export class MonsterSuggestionModal extends SuggestionModal<Monster> {
         return this.items;
     }
     getItemText(item: Monster) {
+        if (this.field) return stringify(item[this.field]) ?? "";
         return item.name;
+    }
+    modifyInput(input: string): string {
+        if (/^@.+\s/.test(input)) {
+            const [_, field] = input.match(/^@(.+?)\s/) ?? [];
+            this.field = (field as keyof Monster) ?? "name";
+        }
+        return input.replace(/^@(.+?)\s/, "").trim();
     }
     onChooseItem(item: Monster) {
         this.inputEl.value = item.name;
@@ -42,25 +52,42 @@ export class MonsterSuggestionModal extends SuggestionModal<Monster> {
             return;
         }
 
-        const matchElements = matches.matches.map((m) => {
-            return createSpan("suggestion-highlight");
-        });
-        for (let i = 0; i < item.name.length; i++) {
-            let match = matches.matches.find((m) => m[0] === i);
-            if (match) {
-                let element = matchElements[matches.matches.indexOf(match)];
-                name.appendChild(element);
-                element.appendText(item.name.substring(match[0], match[1]));
-
-                i += match[1] - match[0] - 1;
-                continue;
-            }
-
-            name.appendText(item.name[i]);
+        if (this.field === "name") {
+            this.renderHighlights(item.name, matches, name);
+        } else {
+            name.appendText(item.name);
         }
         content.createDiv({
             cls: "suggestion-note",
             text: [item.source].flat().join(", ")
         });
+        if (this.field != "name") {
+            let fieldEl = content.createDiv({
+                cls: "suggestion-note"
+            });
+            this.renderHighlights(
+                stringify(item[this.field]),
+                matches,
+                fieldEl
+            );
+        }
+    }
+    renderHighlights(text: string, matches: SearchResult, el: HTMLElement) {
+        const matchElements = matches.matches.map((m) => {
+            return createSpan("suggestion-highlight");
+        });
+        for (let i = 0; i < text.length; i++) {
+            let match = matches.matches.find((m) => m[0] === i);
+            if (match) {
+                let element = matchElements[matches.matches.indexOf(match)];
+                el.appendChild(element);
+                element.appendText(text.substring(match[0], match[1]));
+
+                i += match[1] - match[0] - 1;
+                continue;
+            }
+
+            el.appendText(text[i]);
+        }
     }
 }
