e755ef4bd2c8269ebf4668ddf7fb9f893e103c23
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-11-01 08:59:44 -0400

    fix: Enable section headings on inline groups

--- src/view/ui/SectionHeading.svelte
+++ src/view/ui/SectionHeading.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-    import type { Monster } from "types";
-    import type { TextItem, TraitsItem, GroupItem } from "types/layout";
+    import type { Monster } from "index";
+    import type { TextItem, TraitsItem, GroupItem, InlineItem } from "types/layout";
     import { stringify } from "src/util/util";
     import TextContent from "./TextContent.svelte";
 
     export let monster: Monster;
-    export let item: TraitsItem | TextItem | GroupItem;
+    export let item: TraitsItem | TextItem | GroupItem | InlineItem;
 
     let header: string;
 
