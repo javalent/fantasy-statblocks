be9195fe154708e055a5afc3f8dc08729900f9de
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-03-05 11:20:11 -0500

    chore: Update CI

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -6,6 +6,7 @@ import sveltePreprocess from "svelte-preprocess";
 import inlineWorkerPlugin from "esbuild-plugin-inline-worker";
 import { sassPlugin } from "esbuild-sass-plugin";
 import { config } from "dotenv";
+import { build } from 'tsup';
 
 config();
 
@@ -23,63 +24,78 @@ const prod = process.argv[2] === "production";
 
 const dir = prod ? "./" : process.env.OUTDIR;
 
-esbuild
-    .build({
-        banner: {
-            js: banner
-        },
-        entryPoints: ["src/main.ts", "src/styles.css"],
-        bundle: true,
-        external: [
-            "obsidian",
-            "electron",
-            "codemirror",
-            "@codemirror/autocomplete",
-            "@codemirror/closebrackets",
-            "@codemirror/collab",
-            "@codemirror/commands",
-            "@codemirror/comment",
-            "@codemirror/fold",
-            "@codemirror/gutter",
-            "@codemirror/highlight",
-            "@codemirror/history",
-            "@codemirror/language",
-            "@codemirror/lint",
-            "@codemirror/matchbrackets",
-            "@codemirror/panel",
-            "@codemirror/rangeset",
-            "@codemirror/rectangular-selection",
-            "@codemirror/search",
-            "@codemirror/state",
-            "@codemirror/stream-parser",
-            "@codemirror/text",
-            "@codemirror/tooltip",
-            "@codemirror/view",
-            "@lezer/common",
-            /* "@lezer/lr", */
-            "@lezer/highlight",
-            ...builtins
-        ],
-        format: "cjs",
-        watch: !prod,
-        target: "es2020",
-        logLevel: "info",
-        sourcemap: prod ? false : "inline",
-        minify: prod,
-        treeShaking: true,
-        outdir: dir,
-        plugins: [
-            sassPlugin(),
-            sveltePlugin({
-                compilerOptions: { css: true },
-                preprocess: sveltePreprocess(),
-                filterWarnings: (warning) => {
-                    return !filteredWarnings.includes(warning.code);
-                }
-            }),
-            inlineWorkerPlugin()
-        ]
-    })
-    .catch(() => {
+const params = {
+    banner: {
+        js: banner
+    },
+    entryPoints: ["src/main.ts", "src/styles.css"],
+    bundle: true,
+    external: [
+        "obsidian",
+        "electron",
+        "codemirror",
+        "@codemirror/autocomplete",
+        "@codemirror/closebrackets",
+        "@codemirror/collab",
+        "@codemirror/commands",
+        "@codemirror/comment",
+        "@codemirror/fold",
+        "@codemirror/gutter",
+        "@codemirror/highlight",
+        "@codemirror/history",
+        "@codemirror/language",
+        "@codemirror/lint",
+        "@codemirror/matchbrackets",
+        "@codemirror/panel",
+        "@codemirror/rangeset",
+        "@codemirror/rectangular-selection",
+        "@codemirror/search",
+        "@codemirror/state",
+        "@codemirror/stream-parser",
+        "@codemirror/text",
+        "@codemirror/tooltip",
+        "@codemirror/view",
+        "@lezer/common",
+        /* "@lezer/lr", */
+        "@lezer/highlight",
+        ...builtins
+    ],
+    format: "cjs",
+    target: "es2020",
+    logLevel: "info",
+    sourcemap: prod ? false : "inline",
+    minify: prod,
+    treeShaking: true,
+    outdir: dir,
+    plugins: [
+        sassPlugin(),
+        sveltePlugin({
+            compilerOptions: { css: true },
+            preprocess: sveltePreprocess(),
+            filterWarnings: (warning) => {
+                return !filteredWarnings.includes(warning.code);
+            }
+        }),
+        inlineWorkerPlugin()
+    ]
+};
+
+if (prod) {
+    await esbuild.build(params).catch((x) => {
+        if (x.errors) {
+            console.error(x.errors);
+        } else {
+            console.error(x);
+        }
         process.exit(1);
     });
+    await build({
+        entry: ["./src/api/api.ts"],
+        dts: {
+            only: true
+        }
+    });
+} else {
+    let ctx = await esbuild.context(params);
+    await ctx.watch();
+}
