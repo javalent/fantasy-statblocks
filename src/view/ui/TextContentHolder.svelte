159bc83e856fd1bcce55c69ed03a5828404d7238
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-04-30 19:27:14 -0400

    chore: Code cleanup

--- src/view/ui/TextContentHolder.svelte
+++ src/view/ui/TextContentHolder.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-    import DiceHolder from "./DiceHolder.svelte";
     import MarkdownHolder from "./MarkdownHolder.svelte";
 
     export let property: string = "";
 </script>
 
-<!-- {#if render} -->
-    <MarkdownHolder {property} />
-<!-- {:else}
-    <DiceHolder {property} />
-{/if} -->
+<MarkdownHolder {property} />
